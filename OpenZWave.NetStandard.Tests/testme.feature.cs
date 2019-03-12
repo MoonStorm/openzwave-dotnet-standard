0c6029ae0db141cf620dc734e2d43c0134acc14c
MoonStorm <MoonStorm@gmail.com>
2019-03-11 23:23:01 -0600

    some resources renamed

--- OpenZWave.NetStandard.Tests/testme.feature.cs
+++ OpenZWave.NetStandard.Tests/testme.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace ZWave.NetStandard.Tests
+namespace OpenZWave.NetStandard.Tests
 {
     using TechTalk.SpecFlow;
     
